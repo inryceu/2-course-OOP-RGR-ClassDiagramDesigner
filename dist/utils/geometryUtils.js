b87cd36aa4b94c9b1650b2474cce5437f0ee1643
inryceu <pavel.maluev@gmail.com>
2025-11-30 21:57:40 +0200

    Delete dist directory

--- dist/utils/geometryUtils.js
+++
@@ -1,4 +0,0 @@
-export function calculateAngle(from, to) {
-    return Math.atan2(to.y - from.y, to.x - from.x);
-}
-//# sourceMappingURL=geometryUtils.js.map
--- dist/utils/stringUtils.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function cleanString(str: string): string;
-export declare function escapeRegex(str: string): string;
-export declare function shortenType(type: string, maxLength?: number): string;
-export declare function truncateText(ctx: CanvasRenderingContext2D, text: string, maxWidth: number): string;
-export declare function cleanType(type: string, modifiers?: string[]): string;
-export declare function cleanName(name: string): string;
-export declare function extractContentBetweenAngles(str: string): string | null;
-//# sourceMappingURL=stringUtils.d.ts.map
